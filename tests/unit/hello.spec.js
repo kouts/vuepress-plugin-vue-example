eece7506ca21402672712ee01c57410a45312574
Kouts <koutsaftakis@gmail.com>
2021-02-21 20:13:47 +0200

    Added sample test

---
+++ tests/unit/hello.spec.js
@@ -0,0 +1,12 @@
+import { waitNT, waitRAF } from '../utils';
+
+describe('Hello world', () => {
+
+  it('prints a hello world', () => {
+    console.log = jest.fn();
+    console.log('hello');
+    // The first argument of the first call to the function was 'hello'
+    expect(console.log.mock.calls[0][0]).toBe('hello');
+  });
+
+});
