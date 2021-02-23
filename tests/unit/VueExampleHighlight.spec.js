6183461882e57ecca9b4a0f7cb28b0e7add52371
Kouts <koutsaftakis@gmail.com>
2021-02-23 21:50:47 +0200

    Added tests for VueExampleHighlight

---
+++ tests/unit/VueExampleHighlight.spec.js
@@ -0,0 +1,36 @@
+import { shallowMount } from '@vue/test-utils';
+import VueExampleHighlight from '@/VueExampleHighlight';
+import Prism from 'prismjs';
+jest.mock('prismjs');
+Prism.highlight = jest.fn((code) => code);
+
+describe('VueExampleHighlight', () => {
+
+  const props = {
+    code: '<p>Hello world</p>',
+    language: 'html'
+  };
+
+  it('renders the correct markup', () => {
+    const wrapper = shallowMount(VueExampleHighlight, {
+      propsData: props
+    });
+    const div = wrapper.find(`div.language-${wrapper.props().language}`);
+    const pre = wrapper.find(`pre.language-${wrapper.props().language}`);
+    const code = wrapper.find('code');
+    expect(div.exists()).toBe(true);
+    expect(pre.exists()).toBe(true);
+    expect(code.exists()).toBe(true);
+    expect(code.text()).toContain('Hello world');
+    console.log(wrapper.html());
+  });
+
+  it('renders the correctly', () => {
+    const wrapper = shallowMount(VueExampleHighlight, {
+      propsData: props
+    });
+    expect(wrapper).toMatchSnapshot();
+  });  
+
+});
+
