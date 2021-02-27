5bc4191e7bf661b1fa179a59e101a3252f7cb6f2
kouts <koutsaftakis@gmail.com>
2021-02-27 15:09:09 +0200

    Added tests for SFC sections parsing

--- tests/unit/__mocks__/@dynamic/test-component.text.vue
+++ tests/unit/__mocks__/@dynamic/test-component.text.vue
@@ -1,193 +1,40 @@
 <template>
-  <div>
-    <!-- This is a test comment inside the template part -->
-    <dataset
-      v-slot="{ ds }"
-      :ds-data="users"
-      :ds-sortby="sortBy"
-      :ds-sort-as="{ birthdate: isoDateToDate }"
-      :ds-search-in="['balance', 'birthdate', 'name', 'company', 'email', 'phone', 'address', 'favoriteAnimal']"
-      :ds-search-as="{ birthdate: searchAsEuroDate }"
-    >
-      <div class="row">
-        <div class="col-md-6 mb-2 mb-md-0">
-          <dataset-show />
-        </div>
-        <div class="col-md-6">
-          <dataset-search ds-search-placeholder="Search..." :wait="300" />
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md-12">
-          <div class="table-responsive">
-            <table class="table table-striped d-md-table">
-              <thead>
-                <tr>
-                  <th scope="col">#</th>
-                  <th v-for="(th, index) in cols" :key="th.field" :class="['sort', th.sort]" @click="click($event, index)">
-                    {{ th.name }} <i class="gg-select float-right"></i>
-                  </th>
-                </tr>
-              </thead>
-              <dataset-item tag="tbody">
-                <template v-slot="{ row, rowIndex }">
-                  <tr>
-                    <th scope="row">{{ rowIndex + 1 }}</th>
-                    <td>{{ row.name }}</td>
-                    <td>{{ row.email }}</td>
-                    <td>{{ isoDateToEuroDate(row.birthdate) }}</td>
-                  </tr>
-                </template>
-              </dataset-item>
-            </table>
-          </div>
-        </div>
-      </div>
-      <div class="d-flex flex-md-row flex-column justify-content-between align-items-center">
-        <dataset-info class="mb-2 mb-md-0" />
-        <dataset-pager />
-      </div>
-    </dataset>
-  </div>
+  <!-- This is a test comment inside the template part -->
+  <p class="demo">
+    <button class="btn-primary" @click.prevent="handleClick">
+      <slot></slot>(clicked - {{ count }})
+    </button>
+  </p>
 </template>
 
 <script>
-// This is a test comment inside the script part
 export default {
-  name: 'Example2',
-  data: function () {
+  // This is a test comment inside the script part
+  data() {
     return {
-      users: [
-        {
-          "_id": "EA265B20-45F2-953C-C534-3E2A7862059C",
-          "isActive": false,
-          "onlineStatus": "Do not disturb",
-          "balance": 93683,
-          "birthdate": "1946-07-22",
-          "favoriteColor": "orange",
-          "firstName": "Harper",
-          "lastName": "Nolan",
-          "name": "Harper Nolan",
-          "company": "Tortor At Risus LLC",
-          "email": "amet@posuerevulputate.co.uk",
-          "phone": "(0111) 194 7651",
-          "address": "P.O. Box 298, 5571 Mauris Rd.",
-          "favoriteAnimal": "owl"
-        }
-      ],
-      cols: [
-        {
-          name: 'Name',
-          field: 'name',
-          sort: ''
-        },
-        {
-          name: 'Email',
-          field: 'email',
-          sort: ''
-        },
-        {
-          name: 'Birthdate',
-          field: 'birthdate',
-          sort: ''
-        }
-      ]
+      count: 0,
     };
   },
-  computed: {
-    sortBy () {
-      return this.cols.reduce((acc, o) => {
-        if (o.sort) {
-          o.sort === 'asc' ? acc.push(o.field) : acc.push('-' + o.field);
-        }
-        return acc;
-      }, []);
-    }
-  },
   methods: {
-    click (event, i) {
-      let toset;
-      const sortEl = this.cols[i];
-      if (!event.shiftKey) {
-        this.cols.forEach(o => {
-          if (o.field !== sortEl.field) {
-            o.sort = '';
-          }
-        });
-      }
-      if (!sortEl.sort) {
-        toset = 'asc';
-      }
-      if (sortEl.sort === 'desc') {
-        toset = event.shiftKey ? '' : 'asc';
-      }
-      if (sortEl.sort === 'asc') {
-        toset = 'desc';
-      }
-      sortEl.sort = toset;
+    handleClick() {
+      this.count++;
+      console.log('clicked', this.count);
     },
-    isoDateToEuroDate() {
-      return '';
-    },
-    isoDateToDate() {
-      return '';
-    },
-    searchAsEuroDate() {
-      return '';
-    }
-  }
+  },
 };
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 /* This is a test comment inside the style part */
-.gg-select {
-    box-sizing: border-box;
-    position: relative;
-    display: block;
-    transform: scale(var(--ggs,1));
-    width: 22px;
-    height: 22px;
-}
-
-.gg-select::after,
-.gg-select::before {
-    content: "";
-    display: block;
-    box-sizing: border-box;
-    position: absolute;
-    width: 8px;
-    height: 8px;
-    left: 7px;
-    transform: rotate(-45deg);
-}
-
-.gg-select::before {
-    border-left: 2px solid;
-    border-bottom: 2px solid;
-    bottom: 4px;
-    opacity: 0.3;
-}
-
-.gg-select::after {
-    border-right: 2px solid;
-    border-top: 2px solid;
-    top: 4px;
-    opacity: 0.3;
-}
-
-th.sort {
-  cursor: pointer;
-  user-select: none;
-  &.asc {
-    .gg-select::after {
-      opacity: 1;
-    }
-  }
-  &.desc {
-    .gg-select::before {
-      opacity: 1;
-    }
-  }
+.btn-primary {
+  display: inline-block;
+  font-size: 1.2rem;
+  color: #fff;
+  background-color: #3eaf7c;
+  padding: 0.8rem 1.6rem;
+  border-radius: 4px;
+  transition: background-color 0.1s ease;
+  box-sizing: border-box;
+  border-bottom: 1px solid #389d70;
 }
 </style>
