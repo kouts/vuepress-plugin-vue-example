cb383e2f48db1db89bd268819bd376458c867f94
kouts <koutsaftakis@gmail.com>
2021-02-27 15:09:09 +0200

    Added tests for SFC sections parsing

--- tests/unit/VueExample.spec.js
+++ tests/unit/VueExample.spec.js
@@ -1,20 +1,69 @@
-jest.mock('@dynamic/loadComponent');
-import { shallowMount } from '@vue/test-utils';
+import { mount, shallowMount } from '@vue/test-utils';
 import VueExample from '@/VueExample';
-import { loadComponent, loadComponentAsString } from './__mocks__/@dynamic/loadComponent.js';
+jest.mock('@dynamic/loadComponent');
+import { loadComponent, loadComponentAsString } from '@dynamic/loadComponent';
 
 describe('VueExample', () => {
 
   const props = {
     file: 'test-component.vue'
-  };  
+  };
 
-  it('renders', () => {
+  it('renders correctly', () => {
     const wrapper = shallowMount(VueExample, {
       propsData: props
     });
     const div = wrapper.find(`div.card`);
-    // console.log(wrapper.html());
+    console.log(wrapper.html());
   });
+
+  it('parses the template SFC sections', async () => {
+    const wrapper = shallowMount(VueExample, {
+      propsData: props
+    });
+    const contents = await loadComponentAsString();
+    const parsed = wrapper.vm.parseSfcSection('template', contents);
+    const lines = parsed.split(/\r\n|\r|\n/).length;
+    expect(lines).toBe(7);
+    expect(parsed).toContain('<template>');
+    expect(parsed).toContain('</template>');
+    expect(parsed).not.toContain('<s>');
+    expect(parsed).not.toContain('</s>');
+    expect(parsed).not.toContain('<style');
+    expect(parsed).not.toContain('</style>');
+  });
+
+  it('parses the script SFC sections', async () => {
+    const wrapper = shallowMount(VueExample, {
+      propsData: props
+    });
+    const contents = await loadComponentAsString();
+    const parsed = wrapper.vm.parseSfcSection('script', contents);
+    const lines = parsed.split(/\r\n|\r|\n/).length;
+    expect(lines).toBe(16);
+    expect(parsed).toContain('<script>');
+    expect(parsed).toContain('</script>');
+    expect(parsed).not.toContain('<template>');
+    expect(parsed).not.toContain('</template>');
+    expect(parsed).not.toContain('<style');
+    expect(parsed).not.toContain('</style>');
+  });  
+
+
+  it('parses the style SFC sections', async () => {
+    const wrapper = shallowMount(VueExample, {
+      propsData: props
+    });
+    const contents = await loadComponentAsString();
+    const parsed = wrapper.vm.parseSfcSection('style', contents);
+    const lines = parsed.split(/\r\n|\r|\n/).length;
+    expect(lines).toBe(13);
+    expect(parsed).toContain('<style');
+    expect(parsed).toContain('</style>');
+    expect(parsed).not.toContain('<template>');
+    expect(parsed).not.toContain('</template>');
+    expect(parsed).not.toContain('<script>');
+    expect(parsed).not.toContain('</script>');
+  });    
 
 });
