f5a7c89307b904f2d072b397c19cacb4891a3ed6
Kouts <koutsaftakis@gmail.com>
2021-02-28 21:07:11 +0200

    Added data to example

---
+++ docs/.vuepress/components/Example1.vue
@@ -0,0 +1,20 @@
+<template>
+  <vue-example file="TableGrid" :heroes="heroes" :columns="columns" />
+</template>
+
+<script>
+export default {
+  name: 'Example1',
+  data: function() {
+    return {
+      heroes: [
+        { name: 'Chuck Norris', power: Infinity },
+        { name: 'Bruce Lee', power: 9000 },
+        { name: 'Jackie Chan', power: 7000 },
+        { name: 'Jet Li', power: 8000 }
+      ],
+      columns: ['name', 'power']
+    };
+  }
+};
+</script>
